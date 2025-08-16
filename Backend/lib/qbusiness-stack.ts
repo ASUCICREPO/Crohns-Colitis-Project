588c1f980a50d012b3f6b94e652f20815301d451
KHemanthRaju <rajuhemanth456@gmail.com>
2025-08-16 16:11:45 -0700

    fixing bugs

--- Backend/lib/qbusiness-stack.ts
+++ Backend/lib/qbusiness-stack.ts
@@ -146,6 +146,55 @@ export class QBusinessStack extends cdk.Stack {
       },
     });
 
+    // Web Crawler Role
+    const webCrawlerRole = new iam.Role(this, "WebCrawlerRole", {
+      assumedBy: new iam.ServicePrincipal("qbusiness.amazonaws.com"),
+      inlinePolicies: {
+        WebCrawlerPolicy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              actions: ["qbusiness:BatchPutDocument", "qbusiness:BatchDeleteDocument"],
+              resources: ["*"],
+            }),
+          ],
+        }),
+      },
+    });
+
+    // Web Crawler Data Source
+    const webCrawlerDataSource = new cdk.CfnResource(this, 'WebCrawlerDataSource', {
+      type: 'AWS::QBusiness::DataSource',
+      properties: {
+        ApplicationId: qBusinessApp.ref,
+        IndexId: qBusinessIndex.getAtt('IndexId'),
+        Name: 'WebCrawlerDataSource',
+        RoleArn: webCrawlerRole.roleArn,
+        DataSourceConfiguration: {
+          Type: 'WEBCRAWLER',
+          WebCrawlerConfiguration: {
+            Urls: {
+              SeedUrls: [
+                'https://www.crohnscolitisfoundation.org/',
+                'https://gastro.org/',
+                'https://www.crohnscolitiscommunity.org/crohns-colitis-expert-qa'
+              ]
+            },
+            CrawlDepth: 3,
+            CrawlSubDomain: true,
+            CrawlAllDomain: false,
+            MaxLinksPerUrl: 100,
+            RateLimit: 300,
+            MaxFileSize: 50,
+            HonorRobots: true,
+            CrawlAttachments: true,
+            AuthenticationConfiguration: {
+              AuthenticationType: 'NoAuthentication'
+            }
+          }
+        }
+      },
+    });
+
     // Q Business Retriever
     const qBusinessRetriever = new cdk.CfnResource(this, 'QBusinessRetriever', {
       type: 'AWS::QBusiness::Retriever',
@@ -316,6 +365,11 @@ export class QBusinessStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'QBusinessIndexId', {
       value: qBusinessIndex.getAtt('IndexId').toString(),
       description: 'Q Business Index ID',
+    });
+
+    new cdk.CfnOutput(this, 'QBusinessDataSourceId', {
+      value: webCrawlerDataSource.ref,
+      description: 'Q Business Data Source ID',
     });
 
     new cdk.CfnOutput(this, 'QBusinessDataSourceId', {
