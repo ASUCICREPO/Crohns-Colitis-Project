daf2a41a2f717dff30b33978586a1fdbede44f1b
KHemanthRaju <rajuhemanth456@gmail.com>
2025-08-22 10:22:57 -0700

    Update FloatingChatWidget to restrict horizontal overflow while allowing vertical scrolling

--- frontend/src/Components/FloatingChatWidget.jsx
+++ frontend/src/Components/FloatingChatWidget.jsx
@@ -354,7 +354,7 @@ const FloatingChatWidget = () => {
                   </Box>
                 </Box>
               )}
-              <Box sx={{ flex: 1, minHeight: 0, overflow: 'auto' }}>
+              <Box sx={{ flex: 1, minHeight: 0, overflowY: 'auto', overflowX: 'hidden' }}>
                 <AmazonQChat isExpanded={isExpanded} onClose={toggleChat} />
               </Box>
             </Box>
